6299d1296b02ffdca09b16b23b316a2ae3e2662b
Bhuneshar <imankitkalirawana@gmail.com>
2024-01-13 21:32:56 +0530

    added formik to login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,25 @@
 import { Link } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const Login = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("Invalid email address")
+        .required("Email address is required"),
+      password: Yup.string()
+        .min(6, "Password must be at least 6 characters")
+        .required("Password is required"),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
   return (
     <div>
       <header className="relative mx-10vw mt-24">
@@ -13,7 +32,10 @@ const Login = () => {
               Or sign up for an account.
             </p>
             <div className="mt-14">
-              <form action="" className="mb-10 flex flex-col">
+              <form
+                onSubmit={formik.handleSubmit}
+                className="mb-10 flex flex-col"
+              >
                 <div className="mb-6">
                   <label
                     htmlFor="email"
@@ -23,18 +45,64 @@ const Login = () => {
                   </label>
                   <input
                     type="text"
-                    className="w-full px-10 py-8 rounded-lg bg-slate-800 text-slate-100 focus:outline-none font-medium"
+                    className={`border-2 border-slate-800 w-full px-10 py-8 rounded-lg bg-slate-800 text-slate-100 focus:outline-none font-medium outline outline-transparent focus:outline-blue-400 focus:border-transparent transition-all ${
+                      formik.errors.email && formik.touched.email
+                        ? "border-error"
+                        : ""
+                    }`}
                     placeholder="Email address"
                     autoComplete="off"
                     id="email"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
                   />
+                  {formik.errors.email && formik.touched.email ? (
+                    <div className="mt-2 form-message inline-flex items-center">
+                      <span className="tooltip p-1 rounded-sm ml-2 text-red-400 text-sm">
+                        {formik.errors.email}
+                      </span>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                </div>
+                <div className="mb-6">
+                  <label
+                    htmlFor="password"
+                    className="mb-4 inline-block text-lg text-slate-400"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    className={`border-2 border-slate-800 w-full px-10 py-8 rounded-lg bg-slate-800 text-slate-100 focus:outline-none font-medium outline outline-transparent focus:outline-blue-400 focus:border-transparent transition-all ${
+                      formik.errors.password && formik.touched.password
+                        ? "border-error"
+                        : ""
+                    }`}
+                    placeholder="Password"
+                    autoComplete="off"
+                    id="password"
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                  />
+                  {formik.errors.password && formik.touched.password ? (
+                    <div className="mt-2 form-message inline-flex items-center">
+                      <span className="tooltip p-1 rounded-sm ml-2 text-red-400 text-sm">
+                        {formik.errors.password}
+                      </span>
+                    </div>
+                  ) : (
+                    ""
+                  )}
                 </div>
                 <div className="flex items-center gap-4">
                   <button
-                    disabled
+                    type="submit"
                     className="bg-white text-black rounded-full px-10 text-lg font-medium py-5 disabled:opacity-70"
+                    disabled={formik.isSubmitting}
                   >
-                    Email a login link
+                    Login
                   </button>
                   <Link to="/register" className="underline">
                     Register
